5d9c24bab05f9a69b2d287e4348cdf3c0c5d4509
KIenVu41 <kienvutrung20@gmail.com>
2022-04-11 00:45:32 +0700

    Lesson 1: comment

---
+++ lesson_1/comment/main.js
@@ -0,0 +1,20 @@
+/* 
+1. Mục đích sử dụng
+- Ghi chú: Chúng ta sẽ chú thích trên các đoạn code để hướng dẫn cách sử dụng, mô tả cách hoạt động hoặc giải thích tại sao ta lại làm như vậy. Việc làm này thường áp dụng đối với những đoạn code phức tạp, với những code đơn giản thì không cần sử dụng comment. Hãy áp dụng nguyên tắc đặt tên biến đã học để code của bạn viết ra sẽ tự giải thích luôn ý nghĩa của chính nó.
+- Vô hiệu hóa mã: Bản chất là trình biên dịch code sẽ bỏ qua những comment (không thực thi chúng). Vì vậy khi một đoạn code được comment lại thì đoạn code đó sẽ không chạy. Đôi khi bạn sẽ muốn tạm bỏ đi một đoạn code nào đó trong ứng dụng của bạn, đó chính là lúc bạn có thể sử dụng comment.
+Ví dụ:
+var email = 'email@domain.com';
+var address = 'Hà Nội, Việt Nam';
+
+alert(email); // alert này sẽ chạy
+// alert(address); // alert này không chạy
+
+2. Loại comments
+- Single line comment
+- Multi line comment /** vscode hỗ trợ tạo nhanh
+
+3. Sử dụng phím tắt
+- Windows: Ctrl + /: để tạo comment
+- MacOS: Command + /: để tạo comment
+*/
+
