9c351570e3732d5591be37314c9a39605f670126
KIenVu41 <kienvutrung20@gmail.com>
2022-04-11 00:32:27 +0700

    Lesson 1: variable

---
+++ lesson_1/variable/main.js
@@ -0,0 +1,5 @@
+/* Biến được sử dụng để lưu trữ các thông tin trên trong quá trình ứng dụng Javascript hoạt động.
+có thể viết ; hoặc không ở cuối câu lệnh. */
+var fullName = 'Vu Trung Kien'; // khai báo tên biến và gán giá trị
+var age = 22;
+var address = 'Hà Nội', phone = '0989898989'; // khai báo nhiều biến nhưng không khuyến nghị.
