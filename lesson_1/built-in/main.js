1cbefdcf3837d3aa3707b8f88bd2fee1b85f95a6
KIenVu41 <kienvutrung20@gmail.com>
2022-04-11 01:04:08 +0700

    Lesson 1: built-in function

---
+++ lesson_1/built-in/main.js
@@ -0,0 +1,33 @@
+/*
+*Build-in function: được js xây dựng sẵn để developer sử dụng.
+*Giới thiệu 1 số build-in function:
+*1. alert(): hiển thị thông báo
+*2. prompt(): hiển thị thông báo và nhập giá trị
+*3. confirm(): hiển thị thông báo và nhập giá trị
+*4. Console: hiển thị thông tin trong console
+*5. SetTimeout(): để thực thi 1 function sau một khoảng thời gian ms
+*6. SetInterval(): để thực thi 1 function sau một khoảng thời gian ms
+*/
+
+// alert("Hello World");
+
+// console.log('Đây là 1 dòng log');
+// console.error('Đây là 1 dòng error');
+// console.warn('Đây là 1 dòng warning');
+
+// confirm('Bạn có muốn xóa không?');
+
+// prompt('Nhập tên của bạn');
+
+/*
+setTimeout(function() {
+    alert('Đây là thông báo sau 1s');
+}, 1000); // in ra thông báo sau 1s
+*/
+
+/*
+setInterval(function() {
+    console.log('Đây là thông báo sau 1s');
+}, 5000); // in ra thông báo sau 1s và lặp lại
+*/
+
