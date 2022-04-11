543a49224cd9219ef6f016027137db1b1efc969a
KIenVu41 <kienvutrung20@gmail.com>
2022-04-11 23:03:54 +0700

    Introduce JS operators

---
+++ lesson_2/operators/main.js
@@ -0,0 +1,26 @@
+/**
+ * Giới thiệu về toán tử trong JS
+ * 1. Toán tử số học
+ * 2. Toán tử gán
+ * 3. Toán tử so sánh
+ * 4. Toán tử logic
+ */
+
+var a = 1 + 2;
+console.log(a); // Toán tử số học +
+
+var fullName = 'Nguyễn Văn A';  // = Toán tử gán
+
+var a = 1;
+var b = 2;
+
+if(a > b) { // > Toán tử so sánh
+    alert('a lớn hơn b');
+}
+
+var a = 1;
+var b = 2;
+
+if(a > 0 && b > 0) { // && Toán tử logic
+    alert('a và b lớn hơn 0');
+}
