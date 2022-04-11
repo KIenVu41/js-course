1ede0e54c898013f09722f82377e0ef9da98a3b4
KIenVu41 <kienvutrung20@gmail.com>
2022-04-12 00:13:57 +0700

    assignment

---
+++ lesson_2/assignment/main.js
@@ -0,0 +1,19 @@
+/** Toán tử gán
+
+Toán tử     Ví dụ       Tương đương
+=           x = y       x = y
++=          x += y      x = x + y
+-=          x -= y      x = x - y
+*=          x *= y      x = x * y
+/=          x /= y      x = x / y
+**=         x **= y     x = x ** y  
+ */
+
+var a = 1;
+a += 2;
+console.log(a); // a = a + 2 = 3
+
+var b = 2;
+b **= 2;
+console.log(b); // b = b ** 2 = 4
+
