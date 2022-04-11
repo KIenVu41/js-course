b1d12cee2319184bebf6b45b4c3802d9d898188a
KIenVu41 <kienvutrung20@gmail.com>
2022-04-12 00:43:08 +0700

    string operator

---
+++ lesson_2/string_operator/main.js
@@ -0,0 +1,8 @@
+/**
+ * Toán tử chuỗi - String operator
+ */
+
+var firstName = 'Kien';
+var lastName = 'Vu';
+
+console.log(firstName + ' ' + lastName); // Kien Vu -> + là toán tử số học khi cả 2 vế là số, nhưng chỉ cần 1 trong 2 vế là chuỗi thì + trở thành toán tử nối chuỗi
