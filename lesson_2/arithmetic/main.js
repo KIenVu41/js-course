28eb345be1453689a7d4fade65b0469f4ae034f2
KIenVu41 <kienvutrung20@gmail.com>
2022-04-11 23:48:08 +0700

    arithmetic

---
+++ lesson_2/arithmetic/main.js
@@ -0,0 +1,23 @@
+/**
+ * Toán tử số học
+ * +  -> Cộng
+ * -  -> Trừ
+ * *  -> Nhân
+ * ** -> lũy thừa
+ * /  -> Chia
+ * %  -> Chia lấy phần dư
+ * ++ -> Tăng 1
+ * -- -> Giảm 1
+ */
+
+var a = 1;
+var b = 2;
+
+//var c = a + b;  + c = 3 
+//var c = a - b;  - c = -1
+//var c = a * b;  * c = 2
+//var c = a ** b; ** c = 1
+//var c = a / b;  / c = 0.5
+//var c = a % b;  % c = 1
+// a++; a = 2
+// a--; a = 0
