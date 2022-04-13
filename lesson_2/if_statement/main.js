f506958169144769641dc27b02300677f62336a4
KIenVu41 <kienvutrung20@gmail.com>
2022-04-13 22:20:08 +0700

    If-else

---
+++ lesson_2/if_statement/main.js
@@ -0,0 +1,21 @@
+/**
+ * If - else
+ */
+
+/**
+ * 0
+ * false
+ * '' - ""
+ * undefined
+ * NaM
+ * null
+ * -> convert sang boolean là false, còn lại là true
+ */
+
+var fullName = "Kien Vu"
+
+if (fullName) { // dữ liệu trong khối () là boolean, nếu true thì thực thi các dòng code trong if, ngược lại là else
+    console.log("Dieu kien dung");
+} else {
+    console.log("Dieu kien sai");
+}
