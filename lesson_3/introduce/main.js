2ef9418ad8362fb828a5da4ed32eca4910c76486
KIenVu41 <kienvutrung20@gmail.com>
2022-04-16 23:21:29 +0700

    Introduce function

---
+++ lesson_3/introduce/main.js
@@ -0,0 +1,24 @@
+/**
+ * 1. Hàm
+ * - Một khối mã
+ * - Làm 1 việc cụ thể
+ * 
+ * 2. Loại hàm
+ * - Build-in
+ * - Tự định nghĩa
+ * 
+ * 3. Tính chất
+ * - Không thực thi khi định nghĩa
+ * - Thực thi khi gọi
+ * - Có thể nhận tham số
+ * - Có  thể trả về giá trị
+ */
+
+// Tên hàm có thể có a-z, A-Z, 0-9(không đặt ở đầu tên hàm), _, $
+
+function showDialog() {
+    alert('Hello')
+} 
+
+// call ()
+showDialog()
