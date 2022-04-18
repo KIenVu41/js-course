34ec3c3a613ff7d45b85776203a8c6042fd3ffd8
KIenVu41 <kienvutrung20@gmail.com>
2022-04-18 23:08:49 +0700

    return

---
+++ lesson_3/return/main.js
@@ -0,0 +1,11 @@
+/* 
+    Return trong hàm
+    - Nếu không trả về thì mặc định là undifined
+*/
+
+function add(a, b) {
+    return a + b;
+}
+
+var result = add(10, 20);
+console.log(result);  // 30
