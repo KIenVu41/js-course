002ffcf78b4e04424b79f8a64c776653a29fbce3
KIenVu41 <kienvutrung20@gmail.com>
2022-04-21 21:30:46 +0700

    String methods

--- lesson_4/string/main.js
+++ lesson_4/string/main.js
@@ -30,3 +30,54 @@ var lastName = 'Vu'
 
 console.log(`Toi la ${firstName} ${lastName}`)
 // giá trị trong ${} phải là string
+
+// Làm việc với chuỗi
+
+/*
+var myString = "Hoc JS tai F*!"
+1. Length
+console.log(fullName.length)
+
+2. Find index
+console.log(myString.indexOf("JS"))
+-> Tra ve vi tri ki tu dau tien duoc tim thay
+myString.indexOf("JS", 5) tham so thu 2 tim kiem bat dau tu vi tri thu 5
+-> Khong tim thay tra ve -1
+myString.lastIndexOf("JS")
+-> Vi tri chuoi cuoi cung
+- So sanh voi search(); search cung giong indexOf() nhung khong co param thu 2, ho tro tim kiem theo bieu thuc chinh quy.
+
+3. Cut string
+console.log(myString.slice(0, 5))
+-> Tra ve chuoi tu vi tri thu 0 den vi tri thu 5
+- Neu muon cat het thi chi can dung myString.slice(0), khong can dung tham so thu 2
+- Cat tu phai qua trai dung param am
+
+4. Replace
+console.log(myString.replace("F*", "F9"))
+-> Tra ve chuoi sau khi replace
+- Chi cat tu ki tu dau tien tim thay, muon cat het phai dung regex
+
+5. Convert to upper case
+console.log(myString.toUpperCase())
+-> Chuyen tat ca sang chu 
+
+6. Convert to lower case
+console.log(myString.toLowerCase())
+-> Chuyen tat ca sang chu thuong
+
+7. Trim
+console.log(myString.trim())
+-> Xoa khoang trang dau va cuoi
+
+8. Split
+var languages = "JavaScript, PHP, Ruby, Python, C#, C++"
+console.log(languages.split(","))
+-> Tra ve mang chua cac phan tu cua chuoi duoc cat theo dau ','
+
+9. Get a character by index
+console.log(myString.charAt(0))
+-> Tra ve ki tu theo vi tri trong chuoi, khong co thi tra ve ''
+*/
+
+
